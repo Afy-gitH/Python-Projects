f2c992a83c88670e12ff86bf1eee7de139444900
Afy-gitH <afeefaparveenc@gmail.com>
2023-06-03 08:46:51 +0530

    Update and Correction

--- Computer Guessing Game.py
+++ Computer Guessing Game.py
@@ -3,8 +3,11 @@ def computer_guess(num):
     low = 1
     high = num
     feedback = " "
-    while feedback != 'c':
-        guess = random.randint(low, high)
+     while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # or high, since they are the same
         feedback = input(f"Is {guess} too high (H), toom low (L), or correct (C)").lower()
         if feedback == 'h':
             #the guess is too high , we need to udjust the upper bound
